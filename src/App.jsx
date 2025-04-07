3bb34d151661cb1938c7a5962dad7bf53b41240d
Godfred Nai <godfrednai@gmail.com>
2025-04-07 23:08:52 +0000

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p className="text-4xl text-center font-semibold text-yellow-700">Website Nigaaa!!!!!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
